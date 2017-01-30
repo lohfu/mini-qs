96c9b470d6e25e39b491b8d794c5a22c15449af9
Linus Miller <lohfu@lohfu.io>
2017-01-30 16:18:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mini-qs
+Extremely small, client query string parser aiming for feature parity with Node
---
+++ index.js
@@ -0,0 +1,33 @@
+export function stringify(obj) {
+  return Object.keys(obj).reduce((result, key) => {
+    key = encodeURIComponent(key);
+
+    let value = obj[key];
+
+    if (Array.isArray(value)) return result.concat(value.map((value) => `${key}=${encodeURIComponent(value)}`));
+
+    if (!value || typeof value === 'object') value = '';
+    else value = encodeURIComponent(value);
+
+    result.push(`${key}=${value}`);
+
+    return result;
+  }, []).join('&');
+}
+
+export function parse(str) {
+  return str.replace(/\+/g, ' ').split('&').reduce((result, str) => {
+    const arr = str.split('=');
+
+    if (arr.length === 1) return;
+
+    const [key, val] = arr.map(decodeURIComponent);
+
+    // if there are multiple values per key, concatenate
+    result[key] = key in result ? [].concat(result[key], val) : val;
+
+    return result;
+  }, {});
+}
+
+export default { parse, stringify };
